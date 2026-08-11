761c58677b0dafdbc9675b4b86233abc9da7b0be
superezz <248160976+superezz@users.noreply.github.com>
2026-08-11 13:42:39 +0530

    Authentication completed and now we have to find user data using redis

---
+++ backend/services/auth/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+import { app } from "../configs/firebase.js";
+import { getAuth } from "firebase-admin/auth";
+
+import crypto from "crypto";
+
+export const GoogleAuth = async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const decoded = await getAuth(app).verifyIdToken(token);
+
+
+    let user = await User.findOne({
+      firebaseUid: decoded.uid,
+    });
+
+    if (!user) {
+      user = await User.create({
+        firebaseUid: decoded.uid,
+        name: decoded.name,
+        email: decoded.email,
+      });
+    }
+
+    const sessionId = crypto.randomUUID();
+
+    res.cookie("session", sessionId, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({ success: true, user });
+
+
+
+
+  } catch (error) {
+    return res.status(401).json("Google Authentication Failed");
+  }
+};
